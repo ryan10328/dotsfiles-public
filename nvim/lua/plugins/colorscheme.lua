4296fdb05e93b781f28d9acd42227f75ad9282c0
Ryan Tseng <ryan10328@gmail.com>
2023-12-03 00:28:30 +0800

    remove example and configure colorscheme

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "navarasu/onedark.nvim",
+    lazy = true,
+    name = "onedark",
+    priority = 1000,
+    opts = {
+      style = "darker",
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
+    },
+  },
+}
