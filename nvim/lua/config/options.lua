70d5c460d7e83cf8f771a7990b54831fb123a0b7
Ryan Tseng <ryan10328@gmail.com>
2023-12-04 01:57:59 +0800

    change shell to pwsh if the os is windows

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local is_windows = function()
+  return vim.fn.has("win32")
+end
+
+if is_windows then
+  -- if detect windows, then use powershell instead
+  vim.opt.shell = "pwsh"
+  -- start pwsh and attach -NoLogo as cmd flag
+  vim.opt.shellcmdflag = "-NoLogo"
+end
