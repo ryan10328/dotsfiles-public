a76576e26265c9790d89740e6b8e6bd75050a059
Ryan Tseng <ryan10328@gmail.com>
2023-12-08 02:12:50 +0800

    change the logic of detecting current os

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,11 +2,12 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-local is_windows = function()
-  return vim.fn.has("win32")
+local is_mac = function()
+  return vim.fn.has("macunix")
 end
 
-if is_windows then
+if is_mac then
+else
   -- if detect windows, then use powershell instead
   vim.opt.shell = "pwsh"
   -- start pwsh and attach -NoLogo as cmd flag
