c4705aec6f289e70eca0736ecf59679a82fcc596
Ryan Tseng <ryan10328@gmail.com>
2025-03-29 14:46:29 +0800

    update config

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -25,6 +25,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.vue" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "plugins" },
   },
