a7649dd777110988b63cf123d3d2c6ef71aa3b95
ryan.tseng <ryan10328@gmail.com>
2026-01-02 16:03:49 +0800

    adjust initiate cols and rows

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,9 +1,9 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.initial_cols = 160
-config.initial_rows = 35
-config.font_size = 16.0
+config.initial_cols = 120
+config.initial_rows = 30
+config.font_size = 17.0
 config.window_background_opacity = 0.9
 config.color_scheme = "Bamboo"
 config.font = wezterm.font_with_fallback({
